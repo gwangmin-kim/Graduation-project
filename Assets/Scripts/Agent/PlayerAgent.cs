2932a28cb9a2f7095f294d4c0e834e1f7dd10700
KimGwangmin <gwangmin@snu.ac.kr>
2026-05-08 21:29:31 +0900

    sync

--- Assets/Scripts/Agent/PlayerAgent.cs
+++ Assets/Scripts/Agent/PlayerAgent.cs
@@ -265,7 +265,7 @@ public class PlayerAgent : Agent
         return poseWeight * poseReward + velocityWeight * velocityReward + endEffectorWeight * endEffectorReward;
     }
 
-    private float GetPoseReward(float weight = -0.8f)
+    private float GetPoseReward(float weight = -0.5f)
     {
         float diffSquaredSum = 0f;
         for (int i = 0; i < _jointDriveController.bodyPartList.Count; i++)
@@ -276,18 +276,18 @@ public class PlayerAgent : Agent
             var refbodyPart = _referenceCharacter.bodyPartList[i];
 
             // Pose Error
-            // // 루트(hips) 기준 상대 회전을 비교
-            // var orientation = bodyPart.rigidbody.transform.rotation * Quaternion.Inverse(_hips.rotation);
-            // var refOrientation = refbodyPart.transform.rotation * Quaternion.Inverse(_referenceCharacter.hips.rotation);
-            // float diff = Quaternion.Angle(orientation, refOrientation) * Mathf.Deg2Rad;
+            // 루트(hips) 기준 상대 회전을 비교
+            var orientation = bodyPart.rigidbody.transform.rotation * Quaternion.Inverse(_hips.rotation);
+            var refOrientation = refbodyPart.transform.rotation * Quaternion.Inverse(_referenceCharacter.hips.rotation);
+            float diff = Quaternion.Angle(orientation, refOrientation) * Mathf.Deg2Rad;
 
-            // diffSquaredSum += diff * diff;
+            diffSquaredSum += diff * diff;
 
-            // 변경: 회전이 아니라, 로컬 축의 방향을 비교
-            // 조금 더 느슨한 기준이지만, 현재 모델이 제대로 동작을 재현하지 못하고 있기에 이렇게 시도
-            var orientation = _hips.InverseTransformDirection(bodyPart.rigidbody.transform.up);
-            var refOrientation = _referenceCharacter.hips.InverseTransformDirection(refbodyPart.transform.up);
-            float diff = Vector3.SqrMagnitude(orientation - refOrientation);
+            // // 변경: 회전이 아니라, 로컬 축의 방향을 비교
+            // // 조금 더 느슨한 기준이지만, 현재 모델이 제대로 동작을 재현하지 못하고 있기에 이렇게 시도
+            // var orientation = _hips.InverseTransformDirection(bodyPart.rigidbody.transform.up);
+            // var refOrientation = _referenceCharacter.hips.InverseTransformDirection(refbodyPart.transform.up);
+            // float diff = Vector3.SqrMagnitude(orientation - refOrientation);
 
             diffSquaredSum += diff;
         }
@@ -313,7 +313,7 @@ public class PlayerAgent : Agent
         return Mathf.Exp(weight * diffSquaredSum);
     }
 
-    private float GetEndEffectorReward(float weight = -20f)
+    private float GetEndEffectorReward(float weight = -30f)
     {
         if (_endEffectorList.Count != _referenceCharacter.endEffectorList.Count)
         {
@@ -479,7 +479,8 @@ public class PlayerAgent : Agent
             {
                 reward = 0.6f * targetHeadingReward
                         + 0.1f * taskReward
-                        + 0.3f * balanceReward;
+                        + 0.3f * balanceReward
+                        - 1f;
             }
         }
 
